d24463bac3697c5472b9c7096935a9a09ee242ba
shivprajapat <shivkumarprajapat232@gmail.com>
2023-03-10 18:29:24 +0530

    useRouter-and-withRouter use

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,40 @@
+import { useRouter, withRouter } from "next/router";
 import React from "react";
 
 const Home = () => {
-  return <h1>Home Page</h1>;
+  const router = useRouter();
+  console.log(router);
+
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <button onClick={() => router.push("/profile")}>
+        go to profile (path)
+      </button>
+      <button onClick={() => router.replace("/profile")}>
+        go to profile (replace)
+      </button>
+      <button onClick={() => router.reload("/profile")}>page (reload)</button>
+    </div>
+  );
 };
 
 export default Home;
+
+// const Home = ({router}) => {
+// console.log(router);
+//   return (
+//     <div>
+//       <h1>Home Page</h1>
+//       <button onClick={() => router.push("/profile")}>
+//         go to profile (path)
+//       </button>
+//       <button onClick={() => router.replace("/profile")}>
+//         go to profile (replace)
+//       </button>
+//       <button onClick={() => router.reload("/profile")}>page (reload)</button>
+//     </div>
+//   );
+// };
+
+// export default withRouter(Home);
