1032e074d4b44f8a1463a469457a5ac303116980
shivprajapat <shivkumarprajapat232@gmail.com>
2023-03-16 12:19:51 +0530

    Sass CSS Module Use

--- components/button/index.jsx
+++ components/button/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import styles from './button.module.css'
+import styles from './button.module.scss'
+import variables from '../../styles/variables.module.scss'
 
 const Button = ({ children }) => {
     return (
-        <button type="button" className={styles.primary}>{children}</button>
+        <button type="button" className={styles.primary} style={{color:variables.primaryColor}}>{children}</button>
     )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Button from "../components/button";
-import styles from "../styles/home.module.css"
+import styles from "../styles/home.module.scss"
+import variables from '../styles/variables.module.scss'
+
 const Home = () => {
   return (
     <div className={styles.home}>
       <h1>Next.js complete Course 2023 (🚀)</h1>
       <Button> Hello CSS </Button>
+      <p style={{color:variables.primaryColor}}>Custom CSS variables</p>
     </div>
   );
 };
