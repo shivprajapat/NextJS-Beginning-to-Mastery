fa93f222b36cc31dfe72170b5364f3b8728236ed
shivprajapat <shivkumarprajapat232@gmail.com>
2023-03-16 10:31:30 +0530

    CSS-in-JS Use

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,8 @@
-import { Fragment } from "react";
-import Header from "../components/Header";
 import "../styles/globals.css";
-import 'bootstrap/dist/css/bootstrap.min.css'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Fragment>
-      <Header />
-      <main>
-        <Component {...pageProps} />
-      </main>
-    </Fragment>
+    <Component {...pageProps} />
   );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,25 @@
 import React from "react";
 
 const Home = () => {
-
   return (
     <div>
+      <style jsx global>{`
+        body {
+          background: black;
+          color: white;
+        }
+      `}</style>
       <h1>Next.js complete Course 2023 (🚀)</h1>
+      <style jsx>
+        {`
+          .description {
+            background: red;
+          }
+        `}
+      </style>
+      <div className="description">
+        <p>Hello description</p>
+      </div>
     </div>
   );
 };
