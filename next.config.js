efdd7caa3a911ab0325b91316c8e749fbe284fee
shivprajapat <shivkumarprajapat232@gmail.com>
2023-03-17 12:00:45 +0530

    next.config Redirects Page

--- next.config.js
+++ next.config.js
@@ -2,6 +2,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  redirects: async () => {
+    return [
+      {
+        source: "/about",
+        destination: "/",
+        permanent: true, // Status Code: 308 Permanent Redirect
+        // permanent: false, // Status Code: 307 Temporary Redirect
+
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
